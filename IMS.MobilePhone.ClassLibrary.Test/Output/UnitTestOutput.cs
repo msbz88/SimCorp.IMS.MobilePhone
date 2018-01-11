f8dd1b14092024a646ada9f05a2fc5ec1a99df31
8msbz8 <8msbz8@gmail.com>
2018-01-11 13:20:10 +0200

    Lab2

--- IMS.MobilePhone.ClassLibrary.Test/Output/UnitTestOutput.cs
+++ IMS.MobilePhone.ClassLibrary.Test/Output/UnitTestOutput.cs
@@ -6,44 +6,59 @@ namespace Simcorp.IMS.MobilePhone.ClassLibrary.Test.Output {
     [TestClass]
     public class UnitTestOutput {
         public string Result { get; set;}
-        [TestMethod]
+
         public void TestIPhoneHeadsetPlay(int volume) {
             IPhoneHeadset iPhoneHeadset = new IPhoneHeadset(volume, new TestOutput(this));
             iPhoneHeadset.Play();
             string expectations = iPhoneHeadset.ToString();
             Assert.AreEqual(expectations, Result);
         }
-        [TestMethod]
+    
         public void TestIPhoneHeadsetPlay(int volume, string expectations) {
             IPhoneHeadset iPhoneHeadset = new IPhoneHeadset(volume, new TestOutput(this));
             iPhoneHeadset.Play();
             Assert.AreEqual(expectations, Result);
         }
-        [TestMethod]
+  
         public void TestSamsungHeadsetPlay(int volume) {
             SamsungHeadset samsungHeadset = new SamsungHeadset(volume, new TestOutput(this));
             samsungHeadset.Play();
             string expectations = samsungHeadset.ToString();
             Assert.AreEqual(expectations, Result);
         }
-        [TestMethod]
+    
         public void TestSamsungHeadsetPlay(int volume, string expectations) {
             SamsungHeadset samsungHeadset = new SamsungHeadset(volume, new TestOutput(this));
             samsungHeadset.Play();
             Assert.AreEqual(expectations, Result);
         }
-        [TestMethod]
+
         public void TestPhoneSpeakerPlay(int volume) {
             PhoneSpeaker phoneSpeaker = new PhoneSpeaker(volume, new TestOutput(this));
             phoneSpeaker.Play();
             string expectations = phoneSpeaker.ToString();
             Assert.AreEqual(expectations, Result);
         }
-        [TestMethod]
+ 
         public void TestPhoneSpeakerPlay(int volume, string expectations) {
             PhoneSpeaker phoneSpeaker = new PhoneSpeaker(volume, new TestOutput(this));
             phoneSpeaker.Play();
             Assert.AreEqual(expectations, Result);
+        }
+        [TestMethod]
+        public void ExecuteAllTests() {
+            int samsungVolume = 300;
+            int iphoneVolume = 350;
+            int speakerVolume = 1500;
+            string samsungResultExpectations = $"Audio Type: Samsung Headset (Max volume: {samsungVolume}, current volume: 0%)";
+            string iphoneResultExpectations = $"Audio Type: IPhone Headset (Max volume: {iphoneVolume}, current volume: 0%)";
+            string speakerResultExpectations = $"Audio Type: Phone Speaker (Max volume: {speakerVolume}, current volume: 0%)";
+            TestSamsungHeadsetPlay(samsungVolume);
+            TestSamsungHeadsetPlay(samsungVolume, samsungResultExpectations);
+            TestIPhoneHeadsetPlay(iphoneVolume);
+            TestIPhoneHeadsetPlay(iphoneVolume, iphoneResultExpectations);
+            TestPhoneSpeakerPlay(speakerVolume);
+            TestPhoneSpeakerPlay(speakerVolume, speakerResultExpectations);
         }
     }
 }
