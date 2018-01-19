950741768f9b5ffe9426361697ff0ea651acb53e
8msbz8 <8msbz8@gmail.com>
2018-01-19 14:57:38 +0200

    Lab4

--- IMS.MobilePhone.MessageForm/NewMessageForm.Designer.cs
+++ IMS.MobilePhone.MessageForm/NewMessageForm.Designer.cs
@@ -23,42 +23,70 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonStopMessages = new System.Windows.Forms.Button();
+            this.buttonGenerateMessages = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.richTextBoxNewMessage = new System.Windows.Forms.RichTextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.buttonStopMessages);
+            this.panel1.Controls.Add(this.buttonGenerateMessages);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.richTextBoxNewMessage);
             this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(3, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(260, 237);
+            this.panel1.Size = new System.Drawing.Size(256, 290);
             this.panel1.TabIndex = 0;
+            // 
+            // buttonStopMessages
+            // 
+            this.buttonStopMessages.BackColor = System.Drawing.Color.DarkGray;
+            this.buttonStopMessages.Location = new System.Drawing.Point(107, 30);
+            this.buttonStopMessages.Name = "buttonStopMessages";
+            this.buttonStopMessages.Size = new System.Drawing.Size(140, 21);
+            this.buttonStopMessages.TabIndex = 5;
+            this.buttonStopMessages.Text = "Stop Messages";
+            this.buttonStopMessages.UseVisualStyleBackColor = false;
+            this.buttonStopMessages.Click += new System.EventHandler(this.ButtonStopMesssages);
+            // 
+            // buttonGenerateMessages
+            // 
+            this.buttonGenerateMessages.BackColor = System.Drawing.Color.DarkGray;
+            this.buttonGenerateMessages.Location = new System.Drawing.Point(107, 3);
+            this.buttonGenerateMessages.Name = "buttonGenerateMessages";
+            this.buttonGenerateMessages.Size = new System.Drawing.Size(140, 21);
+            this.buttonGenerateMessages.TabIndex = 4;
+            this.buttonGenerateMessages.Text = "Generate Messages";
+            this.buttonGenerateMessages.UseVisualStyleBackColor = false;
+            this.buttonGenerateMessages.Click += new System.EventHandler(this.ButtonGenerateMessages);
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.button1.Location = new System.Drawing.Point(159, 14);
+            this.button1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.button1.Location = new System.Drawing.Point(167, 72);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 36);
+            this.button1.Size = new System.Drawing.Size(80, 36);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Send Message";
+            this.button1.Text = "Send Own Message";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.ButtonSendMessage);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Location = new System.Drawing.Point(9, 104);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(126, 13);
             this.label2.TabIndex = 3;
@@ -67,7 +95,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Location = new System.Drawing.Point(9, 56);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 13);
             this.label1.TabIndex = 2;
@@ -75,24 +103,28 @@
             // 
             // richTextBoxNewMessage
             // 
-            this.richTextBoxNewMessage.Location = new System.Drawing.Point(12, 71);
+            this.richTextBoxNewMessage.Location = new System.Drawing.Point(9, 120);
             this.richTextBoxNewMessage.Name = "richTextBoxNewMessage";
-            this.richTextBoxNewMessage.Size = new System.Drawing.Size(236, 154);
+            this.richTextBoxNewMessage.Size = new System.Drawing.Size(238, 159);
             this.richTextBoxNewMessage.TabIndex = 1;
             this.richTextBoxNewMessage.Text = "";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Location = new System.Drawing.Point(12, 72);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(126, 20);
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
             this.textBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.GenerateMessages);
             // 
             // NewMessageForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(262, 308);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "NewMessageForm";
@@ -111,5 +143,8 @@
         private System.Windows.Forms.RichTextBox richTextBoxNewMessage;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonGenerateMessages;
+        private System.Windows.Forms.Button buttonStopMessages;
     }
 }
--- IMS.MobilePhone.MessageForm/NewMessageForm.cs
+++ IMS.MobilePhone.MessageForm/NewMessageForm.cs
@@ -1,25 +1,54 @@
 ﻿using Simcorp.IMS.MobilePhone.ClassLibrary.SMS;
 using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Simcorp.IMS.MobilePhone.MessageForm {
     public partial class NewMessageForm : Form {
         TextMessage Message { get; set; }
         FormMessageFormating FormMessageFormating { get; set; }
+        Timer MyTimer = new Timer();
+        private static Random random = new Random();
 
         public NewMessageForm(FormMessageFormating formMessageFormating) {
             this.FormMessageFormating = formMessageFormating;
             InitializeComponent();
             this.Show();
             richTextBoxNewMessage.ScrollToCaret();
+            InitializeTimer(3000);
         }
 
         private void ButtonSendMessage(object sender, EventArgs e) {
-            Message = new TextMessage(textBox1.Text, richTextBoxNewMessage.Text);
             SMSProvider SMSProvider = new SMSProvider(FormMessageFormating);
+            Message = new TextMessage(textBox1.Text, richTextBoxNewMessage.Text);
             SMSProvider.SendMessage(Message);
             richTextBoxNewMessage.Clear();
             richTextBoxNewMessage.Focus();
+        }
+
+        private void ButtonGenerateMessages(object sender, EventArgs e) {
+            MyTimer.Start();
+        }
+
+        private void InitializeTimer(int interval) {
+            MyTimer.Interval = (interval);
+            MyTimer.Tick += new EventHandler(GenerateMessages);
+        }
+
+        public static string RandomString(int length) {
+            const string chars = "ABC";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void GenerateMessages(object sender, EventArgs e) {
+            SMSProvider SMSProvider = new SMSProvider(FormMessageFormating);
+            Message = new TextMessage(RandomString(3), RandomString(15));
+            SMSProvider.SendMessage(Message);
+        }
+
+        private void ButtonStopMesssages(object sender, EventArgs e) {
+            MyTimer.Stop();
         }
     }
 }
