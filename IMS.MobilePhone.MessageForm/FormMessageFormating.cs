adb4a8658a4353ba18a11554b6a556e119ca21b0
8msbz8 <8msbz8@gmail.com>
2018-01-19 12:30:06 +0200

    Lab4

--- IMS.MobilePhone.MessageForm/FormMessageFormating.cs
+++ IMS.MobilePhone.MessageForm/FormMessageFormating.cs
@@ -101,6 +101,7 @@ namespace Simcorp.IMS.MobilePhone.MessageForm {
         private void InitializeComboBoxUsers() {
             comboBoxUniqueUsers.Items.Clear();
             comboBoxUniqueUsers.Items.Add("All");
+            comboBoxUniqueUsers.Text = "All";
             comboBoxUniqueUsers.Items.AddRange(Messages.Select(message => message.User).Distinct().ToArray());
         }
 
